dd82baf8dd13d00a6727466e110f082f9962cb2d
liushuailong <liushuailong@liushuailong.cn>
2021-10-11 15:57:58 +0000

    update dom

--- src/dom.rs
+++ src/dom.rs
@@ -1,18 +1,54 @@
-use std::collections::HashMap;
+// document 数据结构
+use std::collections::{HashMap, HashSet};
 
-struct Node {
+#[derive(Debug)]
+pub struct Node {
     children: Vec<Node>,
     node_type: NodeType,
 }
 
-enum NodeType {
+#[derive(Debug)]
+pub enum NodeType {
     Text(String),
     Element(ElementData),
 }
 
-struct ElementData {
+#[derive(Debug)]
+pub struct ElementData {
     tag_name: String,
     attributes: AttrMap,
 }
 
-type AttrMap = HashMap<String, String>
+pub type AttrMap = HashMap<String, String>
+
+pub fn text(data: String) -> Node {
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Text(data),
+    }
+}
+
+pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData{
+            tag_name: name,
+            attributes: attrs,
+        }),
+    }
+}
+
+impl ElementData {
+    pub fn id(&self) -> Option<&String> {
+        // HashMap的get方法？？？
+        self.attributes.get("id")
+    }
+
+    pub fn classes(&self) -> HashSet<&str> {
+        // match的使用？？？深入理解
+        match self.attributes.get("class") {
+            Some(classList) => classList.split(" ").collect(),
+            None => HashSet::new(),
+        }
+    }
+}
