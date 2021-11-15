0a3b0391f95c440c417946e9678947e80eb8ac34
liushuailong <liushuailong@liushuailong.cn>
2021-11-15 15:38:03 +0000

    layout

--- src/layout.rs
+++ src/layout.rs
@@ -124,7 +124,126 @@ impl<'a> LayoutBox<'a> {
     }
 
     fn layout_block(&mut self, containing_block: Dimensions) {
-        
+        self.calculate_block_width(containing_block);
+        self.layout_block_position(containing_block);
+        self.layout_block_children();
+        self.calculate_block_height();        
+    }
+
+    fn calculate_block_width(&mut self, containing_block: Dimensions) {
+        let style = self.get_style_node();
+        let auto = Keyword("auto".to_string());
+        let mut width = style.value("width").unwrap_or(auto.clone());
+
+        let zero = Length(0.0, Px);
+
+        let mut margin_left  = style.lookup("margin-left", "margin", &zero);
+        let mut margin_right = style.lookup("margin-right", "margin", &zero);
+
+        let border_left = style.lookup("border-left-width", "border-width", &zero);
+        let border_right = style.lookup("border-right-width", "border-width", &zero);
+
+        let padding_left = style.lookup("padding-left", "padding", &zero);
+        let padding_right = style.lookup("padding-right", "padding", &zero);
+
+        let total = sum([&margin_left, &margin_right, &border_left, &border_right, &padding_left, &padding_right, &width].iter().map(|v| v.to_px()));
+
+        if width != auto && total > containing_block.content.width {
+            if margin_left == auto {
+                margin_left = Length(0.0, Px);
+            }
+            if margin_right = auto {
+                margin_right = Length(0.0, Px);
+            }
+        }
+
+        let underflow = containing_block.content.width - total;
+
+        match (width == auto, margin_left == auto, margin_right == auto) {
+            (false, false, false) => {
+                margin_right = Length(margin_right.to_px() + underflow, Px);
+            }
+            (false, false, true) => {
+                margin_right = Length(underflow, Px);
+            }
+            (false, true, false) => {
+                margin_left = Length(underflow, Px);
+            }
+            (true, _, _) => {
+                if margin_left == auto {
+                    margin_left = Length(0.0, Px);
+                }
+                if margin_right == auto {
+                    margin_right = Length(0.0, Px);
+                }
+
+                if underflow >= 0.0 {
+                    width = Length(underflow, Px);
+                } else {
+                    width = Length(0.0, Px);
+                    margin_right = Length(margin_right.to_px() + underflow, Px);
+                }
+            }
+
+            (false, true, true) => {
+                margin_left = Length(underflow / 2.0, Px);
+                margin_right = Length(underflow / 2.0, Px);
+            }
+        }
+
+        let d = &mut self.dimensions;
+        d.content.width = width.to_px();
+        d.padding.left = padding_left.to_px();
+        d.padding.right = padding_right.to_px();
+        d.border.left = border_left.to_px();
+        d.border.right = border_right.to_px();
+        d.margin.left = margin_left.to_px();
+        d.margin.right = margin_right.to_px();
+    }
+
+    fn calculate_block_position(&mut self, containing_block: Dimensions) {
+        let style = self.get_style_node();
+        let d = &mut self.dimensions;
+
+        let zero = Length(0.0, Px);
+
+        d.margin.top = style.lookup("margin-top", "margin", &zero).to_px();
+        d.margin.bottom = style.lookup("margin-bottom", "margin", &zero).to_px();
+
+        d.border.top = style.lookup("border-top-width", "border-width", &zero).to_px();
+        d.border.bottom = style.lookup("padding-bottom", "padding", &zero).to_px();
+
+        d.padding.top = style.lookup("padding-top", "padding", &zero).to_px();
+        d.padding.bottom = style.lookup("padding-bottom", "padding", &zero).to_px();
+
+        d.content.x = containing_block.content.x + d.margin.left + d.border.top + d.padding.top;
+    }
+
+    fn layout_block_children(&mut self) {
+        let d = &mut self.dimensions;
+        for child in &mut self.children {
+            child.layout(*d);
+            d.content.height = d.content.height + child.dimensions.margin_box().height;
+        }
+    }
+
+    fn calculate_block_height(&mut self) {
+        if let Some(Length(h, Px)) = self.get_style_node().value("height") {
+            self.dimensions.content.height = h;
+        }
+    }
+
+    fn get_inline_container(&mut self) -> &mut Layoutbox<'a> {
+        match self.box_type {
+            InlineNode(_) | AnonymousBlock => self,
+            BlockNode(_) => {
+                match self.children.last() {
+                    Some(&LayoutBox { box_type: AnonymousBlock, ..}) => {}
+                    _ => self.children.push(LayoutBox::new(AnonymousBlock))
+                }
+                self.children.last_mut().unwrap()
+            }
+        }
     }
 }
 
